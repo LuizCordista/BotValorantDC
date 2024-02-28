71fb099530beaa299e3ee7a1647027d4f07b0d00
LuizCordista <148589909+LuizCordista@users.noreply.github.com>
2024-02-28 17:55:54 +0000

    First public version

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import os
 import discord
 from discord.ext import commands
 
-from valorant_api import eloluoz, elorandom
+from valorant_api import elovalorant
 
-segredinho = os.environ["Token_key"]
+secret = os.environ["Token_key"]
 
 intents = discord.Intents.all() 
 
@@ -16,39 +16,13 @@ async def on_ready(self):
     print(f"Logado como {self}")
 
 @client.command()
-async def teste(ctx):
-    await ctx.send("Testando o bot aqui") 
-
-@client.command()
-async def master(ctx):
-    await ctx.send("Master o mais trouxa desse servidor")
-
-@client.command()
-async def anamon(ctx):
-    await ctx.send("A mais bela")
-
-@client.command()
-async def tardelli(ctx):
-    await ctx.send("Leleo, vulgo: amaratos, amarantes, love rats e rato.")
-
-@client.command()
-async def luozelo(ctx):
+async def valorantelo(ctx, name, tag):
   embed = discord.Embed(
     colour=discord.Colour.gold(),
-    title=f"Luoz: {eloluoz()[0]}",
-    description=f"{eloluoz()[1]} pontos"
+    title=f"{name}: {elovalorant(name, tag)[0]}",
+    description=f"{elovalorant(name, tag)[1]} pontos"
   )
-  embed.set_thumbnail(url=eloluoz()[2])
+  embed.set_thumbnail(url=elovalorant(name, tag)[2])
   await ctx.send(embed=embed)
 
-@client.command()
-async def elovalorant(ctx, name, tag):
-  embed = discord.Embed(
-    colour=discord.Colour.gold(),
-    title=f"{name}: {elorandom(name, tag)[0]}",
-    description=f"{elorandom(name, tag)[1]} pontos"
-  )
-  embed.set_thumbnail(url=elorandom(name, tag)[2])
-  await ctx.send(embed=embed)
-
-client.run(segredinho)
+client.run(secret)
--- valorant_api.py
+++ valorant_api.py
@@ -1,20 +1,13 @@
 import requests
 
-def eloluoz():
-  response = requests.get("https://api.henrikdev.xyz/valorant/v1/by-puuid/mmr/br/31d656bc-f37f-593e-b6e6-cd77bec0e4c5")
-  return (response.json()["data"]["currenttierpatched"],
-          response.json()["data"]["ranking_in_tier"],
-          response.json()["data"]["images"]["small"])
-
-
-def elorandom(name, tag):
+def elovalorant(name, tag):
   puuid = requests.get(f"https://api.henrikdev.xyz/valorant/v1/account/{name}/{tag}").json()["data"]["puuid"]
   response = requests.get(f"https://api.henrikdev.xyz/valorant/v1/by-puuid/mmr/br/{puuid}")
   return (response.json()["data"]["currenttierpatched"],
           response.json()["data"]["ranking_in_tier"],
           response.json()["data"]["images"]["small"])
 
-print(eloluoz())
+
   
 
 
